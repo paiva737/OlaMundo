c601ec4c55d96457578b1a1b224403674d482daf
paiva737 <rafaelpaiva636@gmail.com>
2024-10-09 21:16:22 -0300

    projeto react

---
+++ src/componentes/PaginaPadrao/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from "componentes/Banner";
+import { Outlet } from "react-router-dom";
+
+export default function PaginaPadrao() {
+    return (
+        <main>
+            <Banner />
+
+            <Outlet />
+        </main>
+    )
+}
