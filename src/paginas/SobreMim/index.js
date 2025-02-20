f645d7d82764616ded0b6aa987b6254c5a36f3f2
paiva737 <rafaelpaiva636@gmail.com>
2025-02-19 22:02:21 -0300

    adicionando readme e atulizando algumas funcoesç clear .

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -25,20 +25,21 @@ export default function SobreMim() {
                 Oi, tudo bem? Desenvolvedor Front-end e estou feliz de te ver por aqui.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
-            </p>
-            <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+            Sou um desenvolvedor web de 21 anos, comprometido e focado em entregar resultados de alta qualidade. A cada dia me encontro mais na área de desenvolvimento web, onde tenho investido meu tempo e energia para aprimorar minhas habilidades e conhecimentos. Sou uma pessoa atenciosa, responsável e organizada, sempre pronta para encarar novos desafios e evoluir profissionalmente.
 
             </p>
             <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+            Minha meta é atuar como desenvolvedor web, onde poderei aplicar estratégias eficazes, aprimorar minhas habilidades em programação e continuar a crescer como profissional e estudante. Estou disposto a me dedicar totalmente e a fazer o que for necessário para alcançar resultados excepcionais. Busco oportunidades que me permitam evoluir e contribuir de forma significativa para o sucesso da equipe e dos projetos em que estiver envolvido.
+            </p>
+            <p className={styles.paragrafo}>
+            Estou disposto a me dedicar integralmente, investindo o esforço e tempo necessários para alcançar resultados excepcionais e contribuir de forma significativa para o sucesso da equipe e dos projetos aos quais eu for designado.
+            </p>
+            <p className={styles.paragrafo}>
+            Esse compromisso constante com o aprendizado reflete o quanto valorizo a evolução profissional e a busca por soluções inovadoras.
+
+            </p>
+            <p className={styles.paragrafo}>
+            lém disso, estou sempre pronto para encarar novos desafios e acredito firmemente que cada projeto é uma oportunidade única de crescimento, tanto profissional quanto pessoal. 
             </p>
         </PostModelo>
     )
